f089777498a8d3f1bc57005491c463c2a13c2695
hidec-7 <hiedematoaho0730127@gmail.com>
2021-05-26 00:18:04 +0900

    first commit

---
+++ PracticeRxSwift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PracticeRxSwift
+//
+//  Created by hideto c. on 2021/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
